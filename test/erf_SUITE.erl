28be708bd9a114704086925707c69e58acca4afb
Miguel Rubinos Rodríguez <mrrubinos@gmail.com>
2026-04-20 09:34:50 +0200

    feat: streaming responses support. (#98)

--- test/erf_SUITE.erl
+++ test/erf_SUITE.erl
@@ -29,7 +29,13 @@ all() ->
         statics,
         swagger_ui,
         start_stop,
-        reload_conf
+        reload_conf,
+        stream,
+        stream_http_1_0,
+        stream_with_postprocess_middleware,
+        stream_producer_crash,
+        stream_non_200_status,
+        callback_crash
     ].
 
 %%%-----------------------------------------------------------------------------
@@ -577,3 +583,310 @@ reload_conf(_Conf) ->
     meck:unload(erf_callback_2),
 
     ok.
+
+stream(_Conf) ->
+    meck:new([erf_callback], [non_strict, no_link]),
+
+    Chunks = [<<"chunk-1">>, <<"chunk-2">>, <<"chunk-3">>],
+    Producer =
+        fun(Send) ->
+            lists:foreach(fun(Chunk) -> ok = Send(Chunk) end, Chunks)
+        end,
+
+    meck:expect(
+        erf_callback,
+        get_foo,
+        fun(_Request) ->
+            {200, [{<<"content-type">>, <<"text/plain">>}], {stream, Producer}}
+        end
+    ),
+
+    {ok, _Pid} = erf:start_link(#{
+        spec_path => filename:join(
+            [code:lib_dir(erf), "test", <<"fixtures/with_refs_oas_3_0_spec.json">>]
+        ),
+        callback => erf_callback,
+        port => 8789,
+        name => erf_server
+    }),
+
+    {ok, {{"HTTP/1.1", 200, "OK"}, Headers, Body}} =
+        httpc:request(
+            get,
+            {"http://localhost:8789/1/foo", []},
+            [],
+            [{body_format, binary}]
+        ),
+
+    ?assertEqual(iolist_to_binary(Chunks), Body),
+    ?assertEqual(
+        "text/plain",
+        proplists:get_value("content-type", Headers)
+    ),
+
+    RawBytes = raw_http_1_1_get(8789, <<"/1/foo">>),
+    ?assertMatch({match, _}, re:run(RawBytes, <<"Transfer-Encoding: chunked">>, [caseless])),
+    ?assertMatch({match, _}, re:run(RawBytes, <<"\r\n7\r\nchunk-1\r\n">>)),
+    ?assertMatch({match, _}, re:run(RawBytes, <<"\r\n7\r\nchunk-2\r\n">>)),
+    ?assertMatch({match, _}, re:run(RawBytes, <<"\r\n7\r\nchunk-3\r\n">>)),
+    ?assertMatch({match, _}, re:run(RawBytes, <<"\r\n0\r\n\r\n$">>)),
+
+    ok = erf:stop(erf_server),
+
+    meck:unload(erf_callback),
+
+    ok.
+
+stream_http_1_0(_Conf) ->
+    meck:new([erf_callback], [non_strict, no_link]),
+
+    meck:expect(
+        erf_callback,
+        get_foo,
+        fun(_Request) ->
+            Producer = fun(Send) -> ok = Send(<<"unreachable">>) end,
+            {200, [{<<"content-type">>, <<"text/plain">>}], {stream, Producer}}
+        end
+    ),
+
+    {ok, _Pid} = erf:start_link(#{
+        spec_path => filename:join(
+            [code:lib_dir(erf), "test", <<"fixtures/with_refs_oas_3_0_spec.json">>]
+        ),
+        callback => erf_callback,
+        port => 8789,
+        name => erf_server
+    }),
+
+    {ok, Sock} = gen_tcp:connect(
+        "localhost",
+        8789,
+        [binary, {active, false}, {packet, http_bin}]
+    ),
+    ok = gen_tcp:send(
+        Sock,
+        <<"GET /1/foo HTTP/1.0\r\nHost: localhost:8789\r\n\r\n">>
+    ),
+    {ok, {http_response, _Version, Status, _Reason}} = gen_tcp:recv(Sock, 0, 5000),
+    gen_tcp:close(Sock),
+
+    ?assertEqual(505, Status),
+
+    ok = erf:stop(erf_server),
+
+    meck:unload(erf_callback),
+
+    ok.
+
+stream_with_postprocess_middleware(_Conf) ->
+    meck:new(
+        [erf_callback, erf_postprocess_middleware],
+        [non_strict, no_link]
+    ),
+
+    Chunks = [<<"alpha">>, <<"beta">>],
+    Producer =
+        fun(Send) ->
+            lists:foreach(fun(Chunk) -> ok = Send(Chunk) end, Chunks)
+        end,
+
+    meck:expect(
+        erf_callback,
+        get_foo,
+        fun(_Request) ->
+            {200, [{<<"content-type">>, <<"text/plain">>}], {stream, Producer}}
+        end
+    ),
+
+    meck:expect(
+        erf_postprocess_middleware,
+        postprocess,
+        fun(_Req, {Status, Headers, Body}) ->
+            {Status, [{<<"x-touched-by-middleware">>, <<"yes">>} | Headers], Body}
+        end
+    ),
+
+    {ok, _Pid} = erf:start_link(#{
+        spec_path => filename:join(
+            [code:lib_dir(erf), "test", <<"fixtures/with_refs_oas_3_0_spec.json">>]
+        ),
+        callback => erf_callback,
+        postprocess_middlewares => [erf_postprocess_middleware],
+        port => 8789,
+        name => erf_server
+    }),
+
+    {ok, {{"HTTP/1.1", 200, "OK"}, Headers, Body}} =
+        httpc:request(
+            get,
+            {"http://localhost:8789/1/foo", []},
+            [],
+            [{body_format, binary}]
+        ),
+
+    ?assertEqual(iolist_to_binary(Chunks), Body),
+    ?assertEqual(
+        "yes",
+        proplists:get_value("x-touched-by-middleware", Headers)
+    ),
+
+    ok = erf:stop(erf_server),
+
+    meck:unload([erf_callback, erf_postprocess_middleware]),
+
+    ok.
+
+stream_producer_crash(_Conf) ->
+    meck:new([erf_callback], [non_strict, no_link]),
+
+    meck:expect(
+        erf_callback,
+        get_foo,
+        fun(_Request) ->
+            BoomProducer = fun(_Send) -> erlang:error(boom) end,
+            {200, [{<<"content-type">>, <<"text/plain">>}], {stream, BoomProducer}}
+        end
+    ),
+
+    {ok, _Pid} = erf:start_link(#{
+        spec_path => filename:join(
+            [code:lib_dir(erf), "test", <<"fixtures/with_refs_oas_3_0_spec.json">>]
+        ),
+        callback => erf_callback,
+        port => 8789,
+        name => erf_server
+    }),
+
+    {ok, {{"HTTP/1.1", 200, "OK"}, _Headers, Body}} =
+        httpc:request(
+            get,
+            {"http://localhost:8789/1/foo", []},
+            [],
+            [{body_format, binary}]
+        ),
+    ?assertEqual(<<>>, Body),
+
+    meck:expect(
+        erf_callback,
+        get_foo,
+        fun(_Request) ->
+            {200, [], <<"bar">>}
+        end
+    ),
+    ?assertMatch(
+        {ok, {{"HTTP/1.1", 200, "OK"}, _, <<"\"bar\"">>}},
+        httpc:request(
+            get,
+            {"http://localhost:8789/1/foo", []},
+            [],
+            [{body_format, binary}]
+        )
+    ),
+
+    ok = erf:stop(erf_server),
+
+    meck:unload(erf_callback),
+
+    ok.
+
+stream_non_200_status(_Conf) ->
+    meck:new([erf_callback], [non_strict, no_link]),
+
+    meck:expect(
+        erf_callback,
+        get_foo,
+        fun(_Request) ->
+            Producer = fun(Send) -> ok = Send(<<"teapot-body">>) end,
+            {418, [{<<"content-type">>, <<"text/plain">>}], {stream, Producer}}
+        end
+    ),
+
+    {ok, _Pid} = erf:start_link(#{
+        spec_path => filename:join(
+            [code:lib_dir(erf), "test", <<"fixtures/with_refs_oas_3_0_spec.json">>]
+        ),
+        callback => erf_callback,
+        port => 8789,
+        name => erf_server
+    }),
+
+    {ok, {{"HTTP/1.1", 200, "OK"}, _Headers, Body}} =
+        httpc:request(
+            get,
+            {"http://localhost:8789/1/foo", []},
+            [],
+            [{body_format, binary}]
+        ),
+    ?assertEqual(<<"teapot-body">>, Body),
+
+    ok = erf:stop(erf_server),
+
+    meck:unload(erf_callback),
+
+    ok.
+
+callback_crash(_Conf) ->
+    meck:new([erf_callback], [non_strict, no_link]),
+
+    meck:expect(
+        erf_callback,
+        get_foo,
+        fun(_Request) ->
+            erlang:error(kaboom)
+        end
+    ),
+
+    {ok, _Pid} = erf:start_link(#{
+        spec_path => filename:join(
+            [code:lib_dir(erf), "test", <<"fixtures/with_refs_oas_3_0_spec.json">>]
+        ),
+        callback => erf_callback,
+        port => 8789,
+        name => erf_server
+    }),
+
+    %% Elli catches the crash and returns a 500. This exercises
+    %% handle_event(request_error, ...) and handle_exception/3.
+    {ok, {{"HTTP/1.1", Status, _}, _Headers, _Body}} =
+        httpc:request(
+            get,
+            {"http://localhost:8789/1/foo", []},
+            [],
+            [{body_format, binary}]
+        ),
+    ?assertEqual(500, Status),
+
+    ok = erf:stop(erf_server),
+
+    meck:unload(erf_callback),
+
+    ok.
+
+%%%-----------------------------------------------------------------------------
+%%% INTERNAL HELPERS
+%%%-----------------------------------------------------------------------------
+raw_http_1_1_get(Port, Path) ->
+    {ok, Sock} = gen_tcp:connect(
+        "localhost",
+        Port,
+        [binary, {active, false}, {packet, raw}]
+    ),
+    ok = gen_tcp:send(
+        Sock,
+        <<
+            "GET ",
+            Path/binary,
+            " HTTP/1.1\r\nHost: localhost:",
+            (integer_to_binary(Port))/binary,
+            "\r\nConnection: close\r\n\r\n"
+        >>
+    ),
+    All = raw_recv(Sock, <<>>),
+    gen_tcp:close(Sock),
+    All.
+
+raw_recv(Sock, Acc) ->
+    case gen_tcp:recv(Sock, 0, 5000) of
+        {ok, Bytes} -> raw_recv(Sock, <<Acc/binary, Bytes/binary>>);
+        {error, closed} -> Acc
+    end.

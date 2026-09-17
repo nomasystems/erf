5108c5861864088cb796e4556e3292f2511e9cae
Jose E. Cribeiro Aneiros <josecriane@gmail.com>
2026-09-17 15:12:51 +0200

    fix(router): Fixed double-encoding the malformed-body 400 response

--- src/erf_router.erl
+++ src/erf_router.erl
@@ -129,13 +129,11 @@ handle(Name, RawRequest) ->
                 end;
             {error, _Reason} ->
                 ContentTypeHeader = string:casefold(<<"content-type">>),
-                ErrorBody = iolist_to_binary(
-                    json:encode(#{
-                        <<"title">> => <<"Bad Request">>,
-                        <<"status">> => 400,
-                        <<"detail">> => <<"Failed to read request">>
-                    })
-                ),
+                ErrorBody = #{
+                    <<"title">> => <<"Bad Request">>,
+                    <<"status">> => 400,
+                    <<"detail">> => <<"Failed to read request">>
+                },
                 ResponseError = {400, [{ContentTypeHeader, <<"application/json">>}], ErrorBody},
                 {ResponseError, RawRequest}
         end,

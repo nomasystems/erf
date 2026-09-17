ad3296c3febc005ecb8b9e322ae0ba4fb628a18e
Jose E. Cribeiro Aneiros <josecriane@gmail.com>
2026-09-17 13:57:00 +0200

    fix(router): accept json requests with parameters or without body

--- src/erf_router.erl
+++ src/erf_router.erl
@@ -889,6 +889,15 @@ load_binary(ModuleName, Bin) ->
             {error, Reason}
     end.
 
+-spec media_type(ContentType) -> MediaType when
+    ContentType :: binary() | undefined,
+    MediaType :: binary() | undefined.
+media_type(undefined) ->
+    undefined;
+media_type(ContentType) ->
+    [MediaType | _Parameters] = binary:split(ContentType, <<";">>),
+    string:trim(MediaType).
+
 -spec postprocess(Request, RawResponse) -> Response when
     Request :: erf:request(),
     RawResponse :: erf:response(),
@@ -936,7 +945,7 @@ preprocess(RawRequest) ->
     Headers = maps:get(headers, RawRequest, []),
     ContentTypeHeader = string:casefold(<<"content-type">>),
     RawBody = maps:get(body, RawRequest, undefined),
-    case proplists:get_value(ContentTypeHeader, Headers, undefined) of
+    case media_type(proplists:get_value(ContentTypeHeader, Headers, undefined)) of
         <<"application/json">> ->
             case RawBody of
                 NonEmptyBinary when is_binary(NonEmptyBinary), byte_size(NonEmptyBinary) > 0 ->
@@ -947,8 +956,8 @@ preprocess(RawRequest) ->
                         error:Reason ->
                             {error, {cannot_decode_body, Reason}}
                     end;
-                _RawBody ->
-                    {error, {cannot_decode_body, invalid_json}}
+                _EmptyBody ->
+                    {ok, RawRequest#{body => undefined}}
             end;
         _ContentType ->
             {ok, RawRequest}
